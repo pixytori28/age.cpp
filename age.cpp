4d2a8ad6c44c34676e0c7178bf56fa8c30e54b76
Victoria Inahuazo <victoria.inahuazo11@myhunter.cuny.edu>
2024-10-22 01:09:31 -0400

    Create age.cpp

---
+++ age.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Enter your age: " << endl; //you need to input your age on this line
+	int age = 0;
+	cin >> age;
+	
+	cout << "Your age is " << age << "." << endl; //prints out your age from this line
+
+    return 0;
+}
